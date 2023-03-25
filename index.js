a35f0dcaf1f433de0f2225eaa3b4e10898a01d02
vincenttommi <vincenttommi@gmail.com>
2023-03-25 12:32:03 +0300

    adding event l

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
+input.addEventListener('click', clickAlert);
+
 function addingEventListener() {
-
-    const input = document.getElementById("button");
-    input.addEventListener('click', function(event){
-
-        return clicked;
-
-    });    
-    }
-
-
+  const input = document.getElementById("button")
+  input.addEventListener('click', function(event) {
+      return "clicked"
+    });
+}
